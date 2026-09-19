d634e4d5560caffed4401e009a69741a454076a0
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-09-19 20:44:33 +0200

    began with impl

--- main.rs
+++ main.rs
@@ -19,6 +19,21 @@ struct DanceCard {
     slots: Vec<DanceSlot>,
 }
 
+impl DanceCard {
+    fn new(owner_name: String) -> Self {
+        let initial_slots = vec![
+                DanceSlot { id: 1, style: DanceStyle::Walzer, partner: None },
+                DanceSlot { id: 2, style: DanceStyle::GermanDance, partner: None },
+                DanceSlot { id: 3, style: DanceStyle::Polka, partner: None },
+            ];
+        
+        Self {
+            owner: owner_name,
+            slots: initial_slots,
+        }
+    }
+}
+
 fn main() {
 
 }
